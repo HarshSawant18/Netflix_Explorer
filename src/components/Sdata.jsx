d2e8fef3fbb504e32dc9a2658f1809ef09a7dae0
HarshSawant18 <hsawant302@gmail.com>
2024-06-26 22:13:21 +0530

    first commit

---
+++ src/components/Sdata.jsx
@@ -0,0 +1,36 @@
+const Sdata = [
+    {
+        sname: "Kota Factory",
+        imgscr: "https://th.bing.com/th/id/OIF.0BfDrhgkc2F3o8gdDTKbSw?rs=1&pid=ImgDetMain",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/81249783"
+    },
+    {
+        sname: "Little Things",
+        imgscr: "https://th.bing.com/th/id/OIP.6kp-JEisb1mliUY8lOBSAAHaEK?rs=1&pid=ImgDetMain",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/81011159"
+    },
+    {
+        sname: "Delhi Crime",
+        imgscr: "https://th.bing.com/th/id/OIP.AC0tVn7B7mkekwvhBm-NCgHaFj?rs=1&pid=ImgDetMain",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/81076756"
+    },
+    {
+        sname: "Squid Game",
+        imgscr: "https://blog.daraz.pk/wp-content/uploads/2021/09/Netflix-Squid-Game.jpg",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/81040344"
+    },
+    {
+        sname: "Money Heist",
+        imgscr: "https://www.stackumbrella.com/wp-content/uploads/2021/09/Feature-image-1330.jpg",
+        title: "Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80192098"
+    },
+  
+
+]
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from './components/Card';
+import Sdata from './components/Sdata';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let count = 0;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+root.render(
+  <>
+    <h1 className="heading">NetFlix Orignal Series </h1>
+    <div className="cards">
+      {
+        Sdata.map((val) => {
+          return (
+            <Card
+              imgsrc={val.imgscr}
+              title={val.title}
+              sname={val.sname}
+              link={val.links}
+            />
+          );
+        })
+
+      }
+    </div>
+  </>
+);
